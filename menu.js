5ff12b11f62638c938550fbf41e30d5b53a803cf
Pedro <pedro_rique17@hotmail.com>
2022-01-15 01:03:17 -0300

    a

---
+++ menu.js
@@ -0,0 +1,39 @@
+var opAtendimento = document.getElementById("op-atendimento");
+var hamb = document.getElementById("hamburguer");
+
+var ativo = true;
+
+hamb.addEventListener("click",function(){
+    ativo = true;
+});
+
+opAtendimento.addEventListener("click", function () {
+
+    if (ativo) ativo = false;
+    else ativo = true;
+
+    var display = "";
+
+    if(ativo) {
+        display = "block";
+    }
+    else {
+        display = "none";
+    }
+
+    document.getElementById("home").style.display = display;
+    document.getElementById("sobreMim").style.display = display;
+    document.getElementById("contato").style.display = display;
+
+    var hrs = document.getElementsByTagName("hr");
+
+    for (let i = 0; i < hrs.length; i++) {
+        hrs[i].style.display = display;
+    }
+
+});
+
+
+
+
+
