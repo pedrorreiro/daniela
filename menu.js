aac0fe71e541b29adc763158454102975becad23
Pedro <pedro_rique17@hotmail.com>
2022-01-27 16:58:01 -0300

    a

--- menu.js
+++ menu.js
@@ -9,28 +9,31 @@ hamb.addEventListener("click",function(){
 
 opAtendimento.addEventListener("click", function () {
 
-    if (ativo) ativo = false;
-    else ativo = true;
-
-    var display = "";
-
-    if(ativo) {
-        display = "block";
+    if(window.matchMedia('(max-device-width: 768px)').matches){
+        if (ativo) ativo = false;
+        else ativo = true;
+    
+        var display = "";
+    
+        if(ativo) {
+            display = "block";
+        }
+        else {
+            display = "none";
+        }
+    
+        document.getElementById("home").style.display = display;
+        document.getElementById("sobreMim").style.display = display;
+        document.getElementById("contato").style.display = display;
+    
+        var hrs = document.getElementsByTagName("hr");
+    
+        for (let i = 0; i < hrs.length; i++) {
+            hrs[i].style.display = display;
+        }
+    
     }
-    else {
-        display = "none";
-    }
-
-    document.getElementById("home").style.display = display;
-    document.getElementById("sobreMim").style.display = display;
-    document.getElementById("contato").style.display = display;
-
-    var hrs = document.getElementsByTagName("hr");
-
-    for (let i = 0; i < hrs.length; i++) {
-        hrs[i].style.display = display;
-    }
-
+   
 });
 
 
